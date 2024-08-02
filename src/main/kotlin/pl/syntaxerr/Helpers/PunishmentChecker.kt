001695d4b3adf00f78b76da435fc2e8e09d8a900
WieszczY <wieszczy85@gmail.com>
2024-08-02 20:10:59 +0200

    Boring production stage

--- src/main/kotlin/pl/syntaxerr/Helpers/PunishmentChecker.kt
+++ src/main/kotlin/pl/syntaxerr/Helpers/PunishmentChecker.kt
@@ -4,6 +4,7 @@ import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerLoginEvent
 import pl.syntaxerr.GuardianX
+import net.kyori.adventure.text.Component
 
 class PunishmentChecker(private val plugin: GuardianX) : Listener {
 
@@ -18,8 +19,14 @@ class PunishmentChecker(private val plugin: GuardianX) : Listener {
             val duration = if (endTime == -1L) "permanent" else plugin.timeHandler.formatTime(((endTime - System.currentTimeMillis()) / 1000).toString())
             if (endTime > System.currentTimeMillis() || endTime == -1L) {
                 val reason = punishment.reason
-                val kickMessage = plugin.messageHandler.getMessage("ban", "kick_message", mapOf("reason" to reason, "time" to duration))
-                event.disallow(PlayerLoginEvent.Result.KICK_BANNED, kickMessage)
+                val kickMessages = plugin.messageHandler.getComplexMessage("ban", "kick_message", mapOf("reason" to reason, "time" to duration))
+                val kickMessage = Component.text()
+                kickMessages.forEach { line ->
+                    kickMessage.append(line)
+                    kickMessage.append(Component.newline())
+                }
+                event.result = PlayerLoginEvent.Result.KICK_BANNED
+                event.kickMessage(kickMessage.build())
             }
         }
     }
